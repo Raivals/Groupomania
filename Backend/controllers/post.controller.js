8ed9b089b740c5c491a6092ab40c954c71a52886
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-09-19 01:45:00 +0200

    Controllers commentaires ajoutés

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -7,7 +7,7 @@ module.exports.readPost = (req, res) => {
     PostModel.find((err, docs) => {
         if (!err) res.send(docs);
         else console.log('Error to get data : ' + err);
-    })
+    }).sort({ createdAt: -1 });
 };
 
 module.exports.createPost = async (req, res) => {
@@ -144,12 +144,53 @@ module.exports.commentPost = (req, res) => {
     } catch (err) {
       return res.status(400).send(err);
     }
-  };
-
+};
+// Modifier un commentaire (facultatif)
 module.exports.editCommentPost = (req, res) => {
-    
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknown : " + req.params.id);
+  
+    try {
+      return PostModel.findById(req.params.id, (err, docs) => {
+        const theComment = docs.comments.find((comment) => {
+          comment._id.equals(req.body.commentId)
+        });
+  
+        if (!theComment) return res.status(404).send("Comment not found");
+        theComment.text = req.body.text;
+  
+        return docs.save((err) => {
+          if (!err) return res.status(200).send(docs);
+          return res.status(500).send(err);
+        });
+      });
+    } catch (err) {
+      return res.status(400).send(err);
+    }
 };
-
+// Supprimer un commentaire (facultatif)
 module.exports.deleteCommentPost = (req, res) => {
-    
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknown : " + req.params.id);
+  
+    try {
+      return PostModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          $pull: {
+            comments: {
+              _id: req.body.commentId,
+            },
+          },
+        },
+        { new: true },
+        (err, docs) => {
+          if (!err) return res.send(docs);
+          else return res.status(400).send(err);
+        }
+      );
+    } catch (err) {
+      return res.status(400).send(err);
+    }
 };
+  
