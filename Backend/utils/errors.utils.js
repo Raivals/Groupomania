70d20f7b07456830595a159fcee6052c881778f3
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-09-18 21:52:10 +0200

    Fin des errors utils

--- Backend/utils/errors.utils.js
+++ Backend/utils/errors.utils.js
@@ -21,3 +21,14 @@ module.exports.signUpErrors = (err) => {
 };
 
 // Erreurs de connexion
+module.exports.signInErrors = (err) => {
+    let errors = { email: '', password: ''}
+
+    if (err.message.includes("email"))
+    errors.email = "Email inconnu";
+
+    if (err.message.includes("password"))
+    errors.password = "Le mot de passe ne correspond pas"
+
+    return errors
+}
