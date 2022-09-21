e4e7aeabffa524dba0a849eb35bb3055ef05099f
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-09-21 11:44:20 +0200

    Création du formulaire d'inscription

--- frontend/src/components/Log/SigninForm.js
+++ frontend/src/components/Log/SigninForm.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+// Pour requêtes http
+import axios from 'axios';
 
-function SigninForm() {
+const SigninForm = () => {
+  // Hook useState
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // Partie logique communication avec le Back
+  const handleLogin = (event) => {
+    // Ne pas rechargert  la page
+    event.preventDefault();
+    
+  };
   return (
-    <div>Connecter</div>
-  )
-}
+    <form action='' onSubmit={handleLogin} id="ssign-up-form">
+      <label htmlFor='email'>Email</label>
+      <br/>
+      <input 
+      type="text" 
+      name='email' 
+      id='email' 
+      onChange={(event) => setEmail(event.target.value)} 
+      value={email} 
+      />
+      <div className='email error'></div>
+      <br/>
+      <label htmlFor='password'>Mot de passe</label>
+      <br/>
+      <input 
+      type="password" 
+      name="password" 
+      id="password"
+      onChange={(event) => setPassword(event.target.value)} 
+      value={password} 
+      />
+      <div className='password error'></div>
+      <br/>
+      <input type="submit" value="Se connecter" />
+    </form>
+  );
+};
 
 export default SigninForm
