cf1528f75be2cbdce714d0dcf949f11248e29485
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-09-22 12:52:23 +0200

    Ajout de la barre de navigation

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+import image from '../assets/img/icons/logo.jpg';
 
-function Navbar() {
+const  Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='nav-container'>
+        <div className='logo'>
+         <NavLink exact to='/'>
+          <div className='logo'>
+            <img src={image} alt='icon'/>
+            <h3>Groupomania</h3>
+          </div>
+         </NavLink> 
+        </div>
+      </div>
+    </nav>
   )
 }
 
