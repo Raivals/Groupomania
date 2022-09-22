ca4ab62d64800f33ab91589f2451786ce1af2e47
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-09-22 12:44:27 +0200

    Fin du formulaire

--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Log from '../components/Log';
-// affichage image ?
+import { UidContext } from '../components/AppContext';
+// affichage image pb avec svg ?
 import image from '../assets/img/login.jpg'
 const Profil = () => {
+// Obteniir l'id utilisateur si celui ci est connecté
+const uid = useContext(UidContext);
+
   return (
     <div className='profil-page'>
+    {uid ? (
+      <h1>UPDATE PAGE</h1>
+    ) : (
+
       <div className='log-container'>
         <Log signin={false} signup={true} />
         <div className='img-container'>
           <img src={image} alt='img-log'/> 
         </div>
-      </div> 
+      </div>
+    )} 
     </div>
   )
 }
