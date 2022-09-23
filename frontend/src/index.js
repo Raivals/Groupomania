ac908d8750b04c655ec6015d457a80af346713de
Romain BERNARDOT <Romain71.contact@gmail.com>
2022-09-23 11:54:56 +0200

    Création du store Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // Import du style du site web
 import './styles/index.scss';
+import { Provider } from 'react-redux';
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// dev tools
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+// Creation du store
+const store = configureStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
